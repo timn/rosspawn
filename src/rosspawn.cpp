21e36d026438a8dff55fe6472594470d8cea729c
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-10-18 12:11:39 -0400

    Added CMU/Intel Labs Pittsburgh to copyright header

--- src/rosspawn.cpp
+++ src/rosspawn.cpp
@@ -4,6 +4,8 @@
  *
  *  Created: Tue Aug  3 17:06:44 2010
  *  Copyright  2010  Tim Niemueller [www.niemueller.de]
+ *             2010  Carnegie Mellon University
+ *             2010  Intel Labs Pittsburgh, Intel Research
  *
  ****************************************************************************/
 
